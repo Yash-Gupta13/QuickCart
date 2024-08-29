dcae699d4b3f2f6498733255b2425ffe5b685b71
Yash Gupta <yashuu1308@gmail.com>
2024-08-30 00:10:37 +0530

    Make the navigation section of the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Outlet} from 'react-router-dom';
+import Navigation from './pages/Auth/Navigation';
+import {ToastContainer} from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>Welcome to the QuickCart</>
+   <>
+    <ToastContainer/>
+    <Navigation/>
+    <main className='py-3'>
+      <Outlet/>
+    </main>
+   </>
   )
 }
 
